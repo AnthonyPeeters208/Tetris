6fc2deda1b55152efe6518c76b6dd6db779b3555
Anthony <peetersanthony208@gmail.com>
2023-07-28 23:00:16 +0200

    Adjust size for mobile users

--- js_src/main.js
+++ js_src/main.js
@@ -1,6 +1,7 @@
 import {TetrisGame} from "./TetrisGame.js";
 
-let canvas_height = Math.round((Math.max(window.innerWidth, window.innerHeight) * 0.4));
+//let canvas_height = Math.round((Math.max(window.innerWidth, window.innerHeight) * 0.4));
+let canvas_height = canvas_height = window.innerHeight * 0.8;
 const tetris_canvas = document.getElementById("tetriscanvas");
 let cell_width =  Math.floor(canvas_height / 20);
 tetris_canvas.height = cell_width * 20;
