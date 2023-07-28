1e51a55b69cd0f31dfc4c7572f2029197a2fcf92
Anthony <peetersanthony208@gmail.com>
2023-07-28 21:08:46 +0200

    You can move a single piece around now

--- js_src/TetrisGame.js
+++ js_src/TetrisGame.js
@@ -16,11 +16,13 @@ class TetrisGame{
         // cell_width: width of a single cell in pixels
         this.ctx = ctx;
         this.cell_width = cell_width;
+        this.game_height = 20;
+        this.game_width = 10;
 
         let matrix_map = []
-        for(let h=-2; h<20; h++){
+        for(let h=-2; h<this.game_height; h++){
             let row = []
-            for(let w=0; w<10; w++){
+            for(let w=0; w<this.game_width; w++){
                 row.push(0);
             }
             matrix_map.push(row);
@@ -58,7 +60,63 @@ class TetrisGame{
 
     isValidMove(){
         //TODO
+        let cp = this.curr_piece;
+        for(let i=0; i<cp.matrix_rep.length; i++){
+            for(let j=0; j<cp.matrix_rep[0].length; j++){
+                let true_x = cp.x+j;    // x coord in the game matrix
+                let true_y = cp.y+i;
+                if(cp.matrix_rep[i][j] !== 0){
+                    // check if actual part of the piece is out of bounds
+                    if(true_x < 0 || true_x >= this.game_width){
+                        console.log("out of x bounds");
+                        return false;
+                    }
+                    if(true_y >= this.game_height){
+                        console.log("out of y bounds");
+                        return false;
+                    }
+                    if(true_y > 0){ // consider starting pieces
+                        // check if piece collides with piece in matrix
+                        if(this.game_matrix[true_y][true_x] !== 0){
+                            console.log("collision with other piece");
+                            return false; // there is a piece already here
+                        }
+                    }
+                }
+            }
+        }
         return true;
+    }
+
+    drawPieces(){
+        // draw pieces placed in the game
+        let cw = this.cell_width;
+        for(let i=0; i<this.game_matrix.length; i++){
+            for(let j=0; j<this.game_matrix[0].length; j++){
+                // draw grid regardless
+                this.ctx.fillStyle = "#1b1b1b"
+                this.ctx.fillRect(j*cw+1, i*cw+1, cw-2, cw-2);
+
+                if(this.game_matrix[i][j] !== 0){
+                    let xval = j*cw;
+                    let yval = i*cw;
+                    // color blocks
+                    this.ctx.fillStyle = this.game_matrix[i][j];
+                    this.ctx.fillRect(xval+1, yval+1, cw-2, cw-2);
+                }
+            }
+        }
+        // draw current piece
+        for(let i=0; i<this.curr_piece.matrix_rep.length; i++){
+            for(let j=0; j<this.curr_piece.matrix_rep[0].length; j++){
+                if(this.curr_piece.matrix_rep[i][j] !== 0){
+                    let xval = (this.curr_piece.x + j)*cw;
+                    let yval = (this.curr_piece.y + i)*cw;
+                    this.ctx.fillStyle = this.curr_piece.color;
+                    this.ctx.fillRect(xval+1, yval+1, cw-2, cw-2);
+                }
+            }
+        }
     }
 
     moveDown(){
@@ -69,6 +127,23 @@ class TetrisGame{
         if(!this.isValidMove()){
             this.curr_piece.y = old_y;
         }
+        this.drawPieces();
+    }
+    moveHorizontal(modifier){
+        // move left (-1) or right (+1)
+        let old_x = this.curr_piece.x;
+        this.curr_piece.x = old_x + modifier;
+
+        if(!this.isValidMove()){
+            this.curr_piece.x = old_x
+        }
+        this.drawPieces();
+    }
+    moveLeft(){
+        this.moveHorizontal(-1);
+    }
+    moveRight(){
+        this.moveHorizontal(+1);
     }
 
     placeCurrPiece(){
@@ -86,32 +161,12 @@ class TetrisGame{
         }
     }
 
-    drawPieces(){
-        // draw pieces using canvas context
-        // cell width in pixels
-        let cw = this.cell_width;
-        for(let i=0; i<this.game_matrix.length; i++){
-            for(let j=0; j<this.game_matrix[0].length; j++){
-                // draw grid regardless
-                this.ctx.fillStyle = "#1b1b1b"
-                this.ctx.fillRect(j*cw+1, i*cw+1, cw-2, cw-2);
 
-                if(this.game_matrix[i][j] !== 0){
-                    let xval = j*cw;
-                    let yval = i*cw;
-                    // color blocks
-                    this.ctx.fillStyle = this.game_matrix[i][j];
-                    this.ctx.fillRect(xval+1, yval+1, cw-2, cw-2);
-                }
-            }
-        }
-    }
 
     dropPiece(){
         this.moveDown();
-        this.placeCurrPiece();
         this.drawPieces();
-        window.requestAnimationFrame(this.dropPiece);
+        console.log("peepo")
     }
 
     start(){
@@ -119,9 +174,7 @@ class TetrisGame{
         this.getNextPiece();
         this.moveDown();
         this.moveDown();
-        this.placeCurrPiece();
         this.drawPieces()
-        window.requestAnimationFrame(this.dropPiece);
 
     }
 
--- js_src/main.js
+++ js_src/main.js
@@ -7,7 +7,33 @@ tetris_canvas.height = cell_width * 20;
 tetris_canvas.width = cell_width * 10;
 console.log("Canvas dim: " + tetris_canvas.height + "x" + tetris_canvas.width);
 
+
+
 // TETRIS GAME
 const ctx = document.getElementById("tetriscanvas").getContext("2d");
 let game = new TetrisGame(ctx, cell_width);
 game.start();
+function dropGamePiece(){
+    game.dropPiece();
+}
+
+window.requestAnimationFrame(dropGamePiece);
+
+/*
+Add event listeners for the keystrokes
+ */
+document.onkeydown = function (e) {
+    switch (e.key) {
+        case 'ArrowUp':
+            // do rotation
+            break;
+        case 'ArrowDown':
+            game.moveDown();
+            break;
+        case 'ArrowLeft':
+            game.moveLeft();
+            break;
+        case 'ArrowRight':
+            game.moveRight();
+    }
+};
